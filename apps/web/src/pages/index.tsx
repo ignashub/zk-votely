b86d1fd45734cca506f157a1f5824fc53eac9596
Apšega,Ignas <i412287@git.fhict.nl>
2023-02-25 20:20:31 +0100

    feat: start of coordinator and verifier workflow

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from 'next';
+import { useRouter } from 'next/router';
 import {
   Text,
   Box,
@@ -13,116 +14,29 @@ import {
 } from '@chakra-ui/react';
 import React, { useState } from 'react';
 import { useContract, useSigner } from 'wagmi';
-import { BigNumber, utils, ethers } from 'ethers';
-import { SemaphoreVotingAbi } from '../abis/SemaphoreVoting';
 import { ConnectButton } from '@rainbow-me/rainbowkit';
-import { Identity } from '@semaphore-protocol/identity';
-import {
-  generateProof,
-  verifyProof as verifyMember,
-} from '@semaphore-protocol/proof';
-import { Group } from '@semaphore-protocol/group';
 
 const Home: NextPage = () => {
-  //SEMAPHORE STUFF
-  const [identity, setIdentity] = useState<Identity>();
-  const [group, setGroup] = useState<Group>(new Group(1));
+  const [signerAddress, setSignerAddress] = useState<string | undefined>();
 
-  const [semaphoreProof, setSemaphoreProof] = useState('');
-
-  const externalNullifier = utils.formatBytes32String('Topic');
-  const snarkArtifactsPath = 'zkproof/../../artifacts/snark';
-
-  //For creating pool
-  const [pollId, setPollId] = useState<BigNumber | undefined>();
-  const [coordinator, setCoordinator] = useState<`0x${string}` | undefined>();
-  const [merkleTreeDepth, setMerkleTreeDepth] = useState<
-    BigNumber | undefined
-  >();
-
-  //Alerts
-  const [successfulAlert, setSuccessfulAlert] = useState(false);
-  const [errorAlert, setErrorAlert] = useState(false);
-  const [loadingAlert, setLoadingAlert] = useState(false);
-
-  //Signer
+  const router = useRouter();
   const { data: signer, isError, isLoading } = useSigner();
 
-  //SemaphoreVote Smart Contract
-  const contract = useContract({
-    address: '0x5b7fAb089fAEbd3f98c96487Cb1ce04a7c44cE44',
-    abi: SemaphoreVotingAbi,
-    signerOrProvider: signer,
-  });
-
-  const setCoordinatorAddress = async () => {
+  const fetchSignerAddress = async () => {
     const address = await signer.getAddress();
-    setCoordinator(`0x${address.slice(2)}`);
-    console.log(coordinator);
+    setSignerAddress(address);
+    console.log(address);
   };
 
-  const createBallout = async () => {
-    const myGasLimit = BigNumber.from(5000000);
-    setLoadingAlert(true);
-
-    try {
-      let result = await contract.createPoll(
-        pollId,
-        coordinator,
-        merkleTreeDepth,
-        {
-          gasLimit: myGasLimit,
-        }
-      );
-      const receipt = await result.wait();
-      // Check the status of the transaction
-      if (receipt.status === 1) {
-        // Transaction succeeded
-        setLoadingAlert(false);
-        setSuccessfulAlert(true);
-        setTimeout(() => {
-          setSuccessfulAlert(false);
-        }, 5000);
-      }
-    } catch (error: any) {
-      // Transaction failed
-      setLoadingAlert(false);
-      setErrorAlert(true);
-      setTimeout(() => {
-        setErrorAlert(false);
-      }, 5000);
+  const goToCoordinatorPage = () => {
+    router.push('/coordinator');
+    query: {
+      add1: signerAddress;
     }
   };
 
-  const createIdentity = () => {
-    const _identity = new Identity();
-    setIdentity(_identity);
-    console.log(_identity.commitment);
-  };
-
-  const joinGroup = () => {
-    console.log(group.members);
-    const newGroup = new Group(1);
-    newGroup.addMember(identity.commitment);
-    setGroup(newGroup);
-  };
-
-  const greeting = utils.formatBytes32String('Hello World');
-
-  const fullProof = async () => {
-    const result = await generateProof(
-      identity,
-      group,
-      externalNullifier,
-      greeting
-      // {
-      //   wasmFilePath: `${snarkArtifactsPath}/semaphore.wasm`,
-      //   zkeyFilePath: `${snarkArtifactsPath}/semaphore.zkey`,
-      // }
-    );
-
-    const verified = await verifyMember(result, 20);
-    console.log(verified);
+  const goToVoterPage = () => {
+    router.push('/voter');
   };
 
   return (
@@ -146,115 +60,36 @@ const Home: NextPage = () => {
           <Box boxSize="750px">
             <Flex justifyContent="center" flexDirection="column">
               <ConnectButton />
-              <Heading size={'xl'} marginTop="50px" marginBottom="20px">
-                Create an Identity
-              </Heading>
-              {identity ? (
-                <Box py="6" whiteSpace="nowrap">
-                  <Box
-                    p="5"
-                    borderWidth={1}
-                    borderColor="gray.500"
-                    borderRadius="4px"
-                  >
-                    <Text textOverflow="ellipsis" overflow="hidden">
-                      Trapdoor: {identity.trapdoor.toString()}
-                    </Text>
-                    <Text textOverflow="ellipsis" overflow="hidden">
-                      Nullifier: {identity.nullifier.toString()}
-                    </Text>
-                    <Text textOverflow="ellipsis" overflow="hidden">
-                      Commitment: {identity.commitment.toString()}
-                    </Text>
-                  </Box>
-                </Box>
-              ) : (
-                <div></div>
-              )}
               <Button
                 variant="solid"
                 bg="black"
                 _hover={{ bg: 'gray.600' }}
                 color="white"
-                onClick={createIdentity}
+                onClick={fetchSignerAddress}
                 marginBottom="16px"
               >
-                Create an Identity
-              </Button>
-              <Input
-                id="outlined-basic"
-                placeholder="Set Ballot Id dont use the same one"
-                type="number"
-                label="BallotId"
-                onChange={(e) => setPollId(BigNumber.from(e.target.value))}
-                errorBorderColor="red.300"
-                style={{ marginBottom: '8px' }}
-              />
-              <Input
-                id="outlined-basic"
-                placeholder="Ballot Size Minimum 16"
-                type="number"
-                label="Ballot Size"
-                onChange={(e) =>
-                  setMerkleTreeDepth(BigNumber.from(e.target.value))
-                }
-                errorBorderColor="red.300"
-                style={{ marginBottom: '8px' }}
-              />
-              <Button
-                variant="solid"
-                bg="black"
-                _hover={{ bg: 'gray.600' }}
-                color="white"
-                onClick={createBallout}
-                marginBottom="16px"
-              >
-                Create a Ballout
+                Set Signer
               </Button>
               <Button
                 variant="solid"
                 bg="black"
                 _hover={{ bg: 'gray.600' }}
                 color="white"
-                onClick={setCoordinatorAddress}
+                onClick={goToCoordinatorPage}
                 marginBottom="16px"
               >
-                Set a Coordinator Address
+                Coordinator
               </Button>
               <Button
                 variant="solid"
                 bg="black"
                 _hover={{ bg: 'gray.600' }}
                 color="white"
-                onClick={joinGroup}
+                onClick={goToVoterPage}
                 marginBottom="16px"
               >
-                Join a Ballout
+                Voter
               </Button>
-              <Button
-                variant="solid"
-                bg="black"
-                _hover={{ bg: 'gray.600' }}
-                color="white"
-                onClick={fullProof}
-                marginBottom="16px"
-              >
-                Create a full proof
-              </Button>
-
-              {successfulAlert && (
-                <Alert status="success" variant="subtle">
-                  <AlertIcon />
-                  <AlertDescription>Successful Transaction!</AlertDescription>
-                </Alert>
-              )}
-              {errorAlert && (
-                <Alert status="error" variant="subtle">
-                  <AlertIcon />
-                  <AlertDescription>Failed Transaction!</AlertDescription>
-                </Alert>
-              )}
-              {loadingAlert && <Spinner />}
             </Flex>
 
             <footer>
